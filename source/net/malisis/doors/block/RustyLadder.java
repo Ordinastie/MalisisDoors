8c584cae202fee52bd11611d3f3eb815287fc9e9
Ordinastie <ordinastie@hotmail.com>
2014-11-20 15:08:58 +0100

    Fixed ladder collision box

--- source/net/malisis/doors/block/RustyLadder.java
+++ source/net/malisis/doors/block/RustyLadder.java
@@ -28,7 +28,9 @@ import net.malisis.doors.MalisisDoors;
 import net.malisis.doors.MalisisDoors.Blocks;
 import net.minecraft.block.BlockLadder;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
 
 /**
  * @author Ordinastie
@@ -52,6 +54,12 @@ public class RustyLadder extends BlockLadder
 	public IIcon getIcon(int side, int metadata)
 	{
 		return Blocks.rustyHatch.getHandleIcon();
+	}
+
+	@Override
+	public AxisAlignedBB getCollisionBoundingBoxFromPool(World p_149668_1_, int p_149668_2_, int p_149668_3_, int p_149668_4_)
+	{
+		return null;
 	}
 
 	@Override
