a2119684aabdee9da55ad4f8e82bca5f173f13e7
Ordinastie <ordinastie@hotmail.com>
2016-09-30 17:12:59 +0200

    Renamed modeldoor to hitechdoor Added proper transforms to hitechdoor

--- src/main/java/net/malisis/doors/block/ModelDoor.java
+++ src/main/java/net/malisis/doors/block/ModelDoor.java
@@ -24,14 +24,18 @@
 
 package net.malisis.doors.block;
 
+import javax.vecmath.Matrix4f;
+
 import net.malisis.core.MalisisCore;
 import net.malisis.core.block.BoundingBoxType;
 import net.malisis.core.block.MalisisBlock;
 import net.malisis.core.block.component.BooleanComponent;
 import net.malisis.core.block.component.DirectionalComponent;
+import net.malisis.core.block.component.ItemTransformComponent;
 import net.malisis.core.renderer.component.AnimatedModelComponent;
 import net.malisis.core.util.AABBUtils;
 import net.malisis.core.util.Timer;
+import net.malisis.core.util.TransformBuilder;
 import net.malisis.core.util.chunkcollision.IChunkCollidable;
 import net.malisis.doors.MalisisDoors;
 import net.minecraft.block.material.Material;
@@ -54,25 +58,41 @@ public class ModelDoor extends MalisisBlock implements IChunkCollidable
 	private static BooleanComponent OPENED = new BooleanComponent("opened", false, 2);
 	private AxisAlignedBB aabb = AABBUtils.identity();
 	private AnimatedModelComponent amc = null;
+	private ItemTransformComponent transformComp = new ItemTransformComponent();
 
 	public ModelDoor()
 	{
 		super(Material.IRON);
-		setName(MalisisDoors.modid + ":modelDoor");
-		setTexture(MalisisDoors.modid + ":blocks/3x2door");
 		setCreativeTab(MalisisDoors.tab);
+
+		//params
+		setName(MalisisDoors.modid + ":hitechdoor");
+		setTexture(MalisisDoors.modid + ":blocks/hitechdoor");
+		aabb = new AxisAlignedBB(-1, 0, 0.375F, 2, 2, 0.625F);
 
 		addComponent(OPENED);
 		addComponent(new DirectionalComponent());
 
-		aabb = new AxisAlignedBB(-1, 0, 0.375F, 2, 2, 0.625F);
-
 		if (MalisisCore.isClient())
 		{
-			amc = new AnimatedModelComponent(MalisisDoors.modid + ":models/3x2door.obj");
+			amc = new AnimatedModelComponent(MalisisDoors.modid + ":models/hitechdoor.obj");
 			amc.onFirstRender(this::stateCheck);
 			addComponent(amc);
+
+			setTransform();
+			addComponent(transformComp);
 		}
+	}
+
+	private void setTransform()
+	{
+		Matrix4f gui = new TransformBuilder().translate(.0F, -0.15F, 0).rotate(30, 45, 0).scale(.34F).get();
+		Matrix4f firstPerson = new TransformBuilder().rotate(0, 135, 0).scale(0.2F).get();
+		Matrix4f thirdPerson = new TransformBuilder().translate(0, 0.155F, -0.1F).rotateAfter(75, 100, 0).scale(0.25F).get();
+		Matrix4f fixed = new TransformBuilder().translate(0, -.2F, 0).scale(0.4F).get();
+		Matrix4f ground = new TransformBuilder().translate(0, 0.3F, 0).scale(0.20F).get();
+
+		transformComp.thirdPerson(thirdPerson, thirdPerson).firstPerson(firstPerson, firstPerson).fixed(fixed).gui(gui).ground(ground);
 	}
 
 	@Override
